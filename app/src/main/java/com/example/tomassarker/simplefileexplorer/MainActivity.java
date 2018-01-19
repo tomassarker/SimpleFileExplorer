4d30b6c906e842b376e215149b5e427abff6775d
TomasSarker <sarker.tom@gmail.com>
2018-01-19 21:09:16 +0100

    Zakladne osetrenie ziskanych povoleni v MainActivity

--- app/src/main/java/com/example/tomassarker/simplefileexplorer/MainActivity.java
+++ app/src/main/java/com/example/tomassarker/simplefileexplorer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tomassarker.simplefileexplorer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -10,10 +15,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Fragment progressBarFragment;
+    private File showedDirectory;
+    private boolean canAccesStorage;
+    //request kody pre povolenia
+    static public final HashMap<String, Integer> permissions = new HashMap<String, Integer>(){};
+    static {
+        permissions.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, 1);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.MainActivity_FrameLayout, progressBarFragment);
         transaction.commit();
+
+        //nacitame predvoleny adresar
+        //TODO: nacitanie z preferences
+        showedDirectory = Environment.getExternalStorageDirectory();
+
+        //overime opravnenie citat/zapisovat ulozisko
+        canAccesStorage = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        if (!canAccesStorage) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            showPath();
+        }
 
     }
 
@@ -49,9 +78,30 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.action_refresh) {
+            showPath();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED) {
+            showPath();
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), "appka nebude fungovat..", 1500).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+    private void showPath() {
+        //TODO
+        for (String f: showedDirectory.list()
+                ) {
+            Toast.makeText(this, f, Toast.LENGTH_SHORT).show();
+//            Snackbar.make(findViewById(android.R.id.content), f, 1500).show();
+        }
+    }
+
 }
