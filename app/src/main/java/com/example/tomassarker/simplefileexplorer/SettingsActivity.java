01ef25834b1fe85461ccc5e5aa37a14b7e7a52f0
TomasSarker <sarker.tom@gmail.com>
2018-01-23 14:23:50 +0100

    Fixnute zmiznutie dialogu na nastavenie predvoleneho priecinku po otoceni obrazovky

--- app/src/main/java/com/example/tomassarker/simplefileexplorer/SettingsActivity.java
+++ app/src/main/java/com/example/tomassarker/simplefileexplorer/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.tomassarker.simplefileexplorer;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,10 +18,14 @@ public class SettingsActivity extends AppCompatActivity {
         currentPath = getIntent().getExtras().getString(CURRENT_PATH);
 
         // Display the fragment as the main content.
-        SettingsFragment settingsFragment = SettingsFragment.newInstance(currentPath);
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, settingsFragment)
-                .commit();
+        Fragment existingFragment = getFragmentManager().findFragmentById(android.R.id.content);
+        //ak uz fragment existuje, nerobime nic, obnovi sa sam
+        if (existingFragment == null || !existingFragment.getClass().equals(SettingsFragment.class)) {
+            SettingsFragment settingsFragment = SettingsFragment.newInstance(currentPath);
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, settingsFragment)
+                    .commit();
+        }
     }
 
     @Override
